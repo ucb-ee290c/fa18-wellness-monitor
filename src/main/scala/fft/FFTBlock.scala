553734ff3fe072d522444758cc44b77638045828
Justin Doong <jbdoong@berkeley.edu>
2018-10-24 10:02:34 -0700

    Replaced copied code with code from FFT repo diplomacyPort2 branch instead of master (?) branch

--- src/main/scala/fft/FFTBlock.scala
+++ src/main/scala/fft/FFTBlock.scala
@@ -2,46 +2,17 @@
 
 package fft
 
-import cde.Parameters
 import chisel3._
 import dsptools._
 import dsptools.numbers._
 import dspjunctions._
 import dspblocks._
-
 import freechips.rocketchip.amba.axi4stream._
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.regmapper._
 import freechips.rocketchip.tilelink._
 
-
-class FFTBlock[T <: Data : Real]()(implicit p: Parameters) extends DspBlock()(p) {
-  def controls = Seq()
-  def statuses = Seq()
-
-  lazy val module = new FFTBlockModule[T](this)
-
-  addStatus("Data_Set_End_Status")
-  addControl("Data_Set_End_Clear", 0.U)
-}
-
-class FFTBlockModule[T <: Data : Real](outer: DspBlock)(implicit p: Parameters)
-  extends GenDspBlockModule[T, T](outer)(p) {
-  val module = Module(new FFT[T])
-
-  module.io.in <> unpackInput(lanesIn, genIn())
-  unpackOutput(lanesOut, genOut()) <> module.io.out
-
-  status("Data_Set_End_Status") := module.io.data_set_end_status
-  module.io.data_set_end_clear := control("Data_Set_End_Clear")
-
-  IPXactComponents._ipxactComponents += DspIPXact.makeDspBlockComponent(baseAddr, uuid, this.name)
-}
-
-// TODO: original or Cordic-like version?
-
-// James's version
 class FFTBlock[T <: Data : Real](val config: FFTConfig[T])(implicit p: Parameters) extends TLDspBlock with TLHasCSR {
   val streamNode = AXI4StreamIdentityNode()
   def csrAddress = AddressSet(0x2000, 0x0fff)
