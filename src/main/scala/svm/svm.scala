98bb11920eaa210008b8ce6eb38a89dbe44288a3
adelsonchua <adelson.chua@berkeley.edu>
2018-10-29 14:00:43 -0700

    added preliminary multi-class support (commented)

--- src/main/scala/svm/svm.scala
+++ src/main/scala/svm/svm.scala
@@ -3,11 +3,13 @@ package svm
 import chisel3._
 import dsptools.numbers._
 import dspjunctions.ValidWithSync
+import scala.collection._
 
 trait SVMParams[T <: Data] {
   val protoData: T
   val nSupports: Int
   val nFeatures: Int
+  //val nClasses:  Int
 }
 
 class SVMIO[T <: Data](params: SVMParams[T]) extends Bundle {
@@ -17,6 +19,9 @@ class SVMIO[T <: Data](params: SVMParams[T]) extends Bundle {
   val supportVector = Input(Vec(Vec(params.nFeatures, params.protoData.cloneType), params.nSupports))
   val alphaVector = Input(Vec(params.nSupports, params.protoData.cloneType))
   val intercept = Input(params.protoData.cloneType)
+
+  //val alphaVector = Input(Vec(Vec(params.nSupports, params.protoData.cloneType), params.nClasses*(params.nClasses - 1)/2))
+  //val intercept = Input(Vec(params.nClasses*(params.nClasses - 1)/2, params.protoData.cloneType))
 
   override def cloneType: this.type = SVMIO(params).asInstanceOf[this.type]
 }
@@ -36,6 +41,32 @@ class SVM[T <: chisel3.Data : Real](val params: SVMParams[T]) extends Module {
   // multiply by the weights, add the intercept, clamp it to either 0 or 1, you're done
   // for binary classification this is easy
   io.out.bits := io.alphaVector.zip(kernel).map{ case (a,b) => a * b}.reduce(_ + _) + io.intercept
+  
+  /*
+  // multi-class support, creating votes for every classifier generated
+  val nClassifiers = params.nClasses*(params.nClasses - 1)/2
+
+  val votes = Wire(Vec(nClassifiers, params.protoData.cloneType))
+  val decision = Wire(Vec(nClassifiers, params.protoData.cloneType))
+  for (i <- 0 until nClassifiers) {
+    //votes(i) := (io.alphaVector(i).zip(kernel).map{ case (a,b) => a * b}.reduce(_ + _) + io.intercept(i)) > 0
+    votes(i) := io.alphaVector(i).zip(kernel).map{ case (a,b) => a * b}.reduce(_ + _) + io.intercept(i)
+
+
+    when(votes > ConvertableTo[T].fromInt(0)) {
+      decision(i) := ConvertableTo[T].fromInt(1)
+    } .otherwise {
+      decision(i) := ConvertableTo[T].fromInt(1)
+    }
+  }
+
+  val combinations = mutable.ArrayBuffer[mutable.ArrayBuffer[T]]()
+  for (x <- 0 until params.nClasses) {
+    for (y <- x + 1 until params.nClasses) {
+      combinations += mutable.ArrayBuffer(ConvertableTo[T].fromInt(x),ConvertableTo[T].fromInt(y))
+    }
+  }
+  */
   io.out.valid := io.in.valid
   io.out.sync := io.in.sync
 }
