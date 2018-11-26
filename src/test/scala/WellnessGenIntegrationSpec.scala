fedaa8b6c461316467085aefeb97ed253666f339
ryan <ryankaveh@berkeley.edu>
2018-11-25 16:34:27 -0800

    started work on generator version of top module

---
+++ src/test/scala/WellnessGenIntegrationSpec.scala
@@ -0,0 +1,62 @@
+package wellness
+import chisel3._
+import chisel3.core.FixedPoint
+import dsptools.numbers._
+import features._
+import fft._
+import firFilter._
+import org.scalatest.{FlatSpec, Matchers}
+import pca._
+import svm._
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.{Seq, mutable}
+
+abstract class filterParamsTemplate {
+  val taps:Seq[Double]
+}
+
+
+class wellnessGenIntegrationParameterBundle {
+
+  // the numbers here are actually irrelevant, you just need to put something
+  // these will be overwritten anyway in the WellnessIntegrationSpec class
+  // the 'requires' are not even being checked!
+  val filter1Params:filterParamsTemplate = new filterParamsTemplate {
+    val taps = Seq(0.toDouble)
+  }
+}
+
+class wellnessGenIntegrationSpec extends FlatSpec with Matchers {
+  behavior of "Wellness"
+
+
+  // I am leaving this SInt implementation as is, since my testbench is meant for floats/fixedpoints
+  it should "pass the input through filters, compute features, and classify (SInt)" in {
+
+    val debug = 0
+
+    val windowLength = 4
+
+    val tap_count = 5
+    val coefficients1 = mutable.ArrayBuffer[Int]()
+    for(j <- 0 until tap_count) {
+      coefficients1 += j
+       }
+
+    val goldenModelParameters = new wellnessGenIntegrationParameterBundle {
+      override val filter1Params: filterParamsTemplate = new filterParamsTemplate {
+        val taps: Seq[Double] = coefficients1.map(_.toDouble)
+      }
+    }
+
+    val filter1Params = new FIRFilterParams[SInt] {
+      val protoData = SInt(64.W)
+      val taps = coefficients1.map(_.asSInt())
+    }
+
+    wellnessGenIntegrationTesterSInt(filter1Params: FIRFilterParams[SInt],
+      3,
+      goldenModelParameters: wellnessGenIntegrationParameterBundle, debug) should be (true)
+  }
+}
---
+++ src/test/scala/WellnessGenIntegrationTester.scala
@@ -0,0 +1,62 @@
+package wellness
+
+import breeze.math.Complex
+import chisel3._
+import chisel3.experimental.FixedPoint
+import chisel3.util.log2Ceil
+import dsptools.DspTester
+import dsptools.numbers._
+import firFilter._
+import fft._
+import features._
+import pca._
+import memorybuffer._
+import svm._
+import freechips.rocketchip.config.Parameters
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.Seq
+
+class wellnessGenTester[T <: chisel3.Data](c: wellnessGenModule[T],
+                                           goldenModelParameters: wellnessGenIntegrationParameterBundle,
+                                           dataBP: Int, testType: Int) extends DspTester(c) {
+
+  // Instantiate golden models
+  val filter1 = new GoldenDoubleFIRFilter(goldenModelParameters.filter1Params.taps)
+
+  for (i <- 0 until 100) {
+    var filter1Result = filter1.poke(0)
+
+    var input = scala.util.Random.nextInt(16)
+    filter1Result = filter1.poke(input)
+
+    poke(c.io.in.bits, input)
+    poke(c.io.in.valid, 1)
+    step(1)
+
+    expect(c.io.otherOut, filter1Result)
+  }
+}
+
+
+object wellnessGenIntegrationTesterSInt {
+  implicit val p: Parameters = null
+  def apply(filter1Params: FIRFilterParams[SInt],
+            x: Int,
+            goldenModelParameters: wellnessGenIntegrationParameterBundle, debug: Int): Boolean = {
+    if (debug == 1) {
+      chisel3.iotesters.Driver.execute(Array("-tbn", "firrtl", "-fiwv"), () => new wellnessGenModule(
+        filter1Params: FIRFilterParams[SInt],
+        x: Int)) {
+        c => new wellnessGenTester(c, goldenModelParameters, 0,0)
+      }
+    } else {
+      dsptools.Driver.execute(() => new wellnessGenModule(
+        filter1Params: FIRFilterParams[SInt],
+        x: Int),
+        TestSetup.dspTesterOptions) {
+        c => new wellnessGenTester(c, goldenModelParameters, 0, 0)
+      }
+    }
+  }
+}
