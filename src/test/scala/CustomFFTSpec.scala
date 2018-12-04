e1aa23f24f26233dc004e24585a16b3caaa68966
Justin Doong <jbdoong@berkeley.edu>
2018-12-04 10:16:55 -0800

    Added FFT tests

--- src/test/scala/CustomFFTSpec.scala
+++ src/test/scala/CustomFFTSpec.scala
@@ -8,26 +8,33 @@ import org.scalatest.{FlatSpec, Matchers}
 class CustomFFTSpec extends FlatSpec with Matchers {
   behavior of "FFT"
 
-  // FFT points, lanes, total width, fractional bits, pipeline depth
-  val test = Seq(32, 32, 32, 8, 0)
-  // val test = Seq(64, 64, 62, 16, 0) // this works
-  // val test = Seq(64, 64, 64, 16, 0) // this doesn't work
-  val totalWidth = test(2)
-  val fractionalBits = test(3)
-
-  val config = FFTConfig(
-    genIn = DspComplex(FixedPoint(totalWidth.W, fractionalBits.BP), FixedPoint(totalWidth.W, fractionalBits.BP)),
-    genOut = DspComplex(FixedPoint(totalWidth.W, fractionalBits.BP), FixedPoint(totalWidth.W, fractionalBits.BP)),
-    n = test(0),
-    lanes = test(1),
-    pipelineDepth = test(4),
-    quadrature = false,
+  val tests = Seq(
+    // FFT points, lanes, total width, fractional bits, pipeline depth
+    Seq(32, 32, 32, 8, 0),
+    Seq(64, 64, 62, 16, 0),
+    Seq(128, 128, 35, 19, 0),
+    // Seq(64, 64, 62, 16, 0), // this works
+    // Seq(64, 64, 64, 16, 0), // this doesn't work
   )
 
   it should "FFT" in {
-    val debug = 0
-    val dataWidth = totalWidth
-    val dataBP = fractionalBits
-    FixedPointFFTTester(config, dataBP, debug) should be (true)
+    for (test <- tests) {
+      val totalWidth = test(2)
+      val fractionalBits = test(3)
+
+      val config = FFTConfig(
+        genIn = DspComplex(FixedPoint(totalWidth.W, fractionalBits.BP), FixedPoint(totalWidth.W, fractionalBits.BP)),
+        genOut = DspComplex(FixedPoint(totalWidth.W, fractionalBits.BP), FixedPoint(totalWidth.W, fractionalBits.BP)),
+        n = test(0),
+        lanes = test(1),
+        pipelineDepth = test(4),
+        quadrature = false,
+      )
+
+      val debug = 0
+      val dataWidth = totalWidth
+      val dataBP = fractionalBits
+      FixedPointFFTTester(config, dataBP, debug) should be(true)
+    }
   }
 }
