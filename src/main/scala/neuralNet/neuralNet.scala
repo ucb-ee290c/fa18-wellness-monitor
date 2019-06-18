30601107d78b7dadc217a62b1b4b9c27ac848133
jnozik <nozik@berkeley.edu>
2019-06-18 15:25:10 -0700

    add layer parameterization

--- src/main/scala/neuralNet/neuralNet.scala
+++ src/main/scala/neuralNet/neuralNet.scala
@@ -9,26 +9,19 @@ import dspjunctions.ValidWithSync
 trait NeuralNetParams[T <: Data] {
   val protoData: T
   val nFeatures: Int // number of dimensions in input data
-  val nNeurons: Int // number of neurons in hidden layer
+  val nNeurons: Int // number of neurons in each hidden layer
+  val nLayers: Int // number of hidden layers
 }
 
 class NeuralNetIO[T <: Data](params: NeuralNetParams[T]) extends Bundle {
-  // for all matrices/vectors, assume that chisel Vec shape is like an array, so
-  // that Vec(n, params.protoData) declares a row vector of shape (1, n)
-
-  // input row vector, shape: (1, params.nFeatures)
   val in = Flipped(ValidWithSync(Vec(params.nFeatures, params.protoData)))
-  // output classification
   val out = ValidWithSync(UInt(1.W))
 
-  // weight matrix to connect to hidden layer, shape: (params.nFeatures, params.nNeurons)
-  val weightMatrix = Input(Vec(params.nNeurons, Vec(params.nFeatures, params.protoData)))
-  // weight column vector to connect to output layer, shape: (params.nNeurons, 1)
-  val weightVec = Input(Vec(params.nNeurons, params.protoData))
-
-  // bias row vector, shape: (1, params.nNeurons). this is the transpose of the sklearn implementation
-  val biasVec = Input(Vec(params.nNeurons, params.protoData))
-  val biasScalar = Input(params.protoData)
+  val inputWeights = Input(Vec(params.nNeurons, Vec(params.nFeatures, params.protoData)))
+  val midAndOutputWeights = Input(Vec(params.nNeurons * (params.nLayers - 1) + 1,
+    Vec(params.nNeurons, params.protoData)))
+  val biasVecs = Input(Vec(params.nLayers, Vec(params.nNeurons, params.protoData)))
+  val outputBias = Input(params.protoData)
 
   // non-normalized neural network output for debugging
   val rawVotes = Output(params.protoData)
@@ -41,41 +34,40 @@ object NeuralNetIO {
 }
 
 class NeuralNet[T <: chisel3.Data : Real](val params: NeuralNetParams[T]) extends Module {
+  require(params.nFeatures >= 1, f"Must have at least 1 feature, currently ${params.nFeatures}")
   require(params.nNeurons >= 1, f"Must have at least 1 neuron, currently ${params.nNeurons}")
+  require(params.nLayers >= 1, f"Must have at least 1 hidden layer, currently ${params.nLayers}")
 
   val io = IO(new NeuralNetIO[T](params))
 
+  val hiddenLayers = Wire(Vec(params.nLayers, Vec(params.nNeurons, params.protoData)))
 
-  // multiply input vector by weight matrix
-  val inputWeighted = Wire(Vec(params.nNeurons, params.protoData)) // shape: (1, params.nNeurons)
+  // compute first hidden layer
+
   for (i <- 0 until params.nNeurons) {
-    inputWeighted(i) := io.in.bits.zip(io.weightMatrix(i)).map{ case (a,b) => a * b}.reduce(_ + _)
+    val linearCombo = io.in.bits.zip(io.inputWeights(i)).map{ case (a,b) => a * b}.reduce(_ + _) +
+      io.biasVecs(0)(i)
+    hiddenLayers(0)(i) := Mux(linearCombo > 0, linearCombo, ConvertableTo[T].fromInt(0))
   }
 
-  // add bias vector to weight matrix multiplication result
-  val biased = Wire(Vec(params.nNeurons, params.protoData))
-  biased := inputWeighted.zip(io.biasVec).map{ case (a,b) => a + b}
+  // compute remaining hidden layers, if they exist
 
-  // apply ReLU for final hidden layer
-  val hiddenLayer = Wire(Vec(params.nNeurons, params.protoData))
-  for (i <- 0 until params.nNeurons) {
-    hiddenLayer(i) := Mux(biased(i) > 0, biased(i), ConvertableTo[T].fromInt(0))
+  for (i <- 1 until params.nLayers) {
+    for (j <- 0 until params.nNeurons) {
+      val linearCombo = hiddenLayers(i-1).zip(io.midAndOutputWeights((i-1)*params.nNeurons+j)).map
+      { case (a,b) => a * b}.reduce(_ + _) + io.biasVecs(i)(j)
+      hiddenLayers(i)(j) := Mux(linearCombo > 0, linearCombo, ConvertableTo[T].fromInt(0))
+    }
   }
 
-  // compute dot product of hidden layer and weight vector
-  val dotProduct = Wire(params.protoData)
-  for (i <- 0 until params.nFeatures) {
-    dotProduct := hiddenLayer.zip(io.weightVec).map{ case (a,b) => a * b}.reduce(_ + _)
-  }
+  // compute output layer
 
-  val actualPreReLU = Wire(params.protoData)
-  actualPreReLU := dotProduct + io.biasScalar
-  val actualVotes = Wire(params.protoData)
-  // apply ReLU for raw neural net output
-  actualVotes := Mux(actualPreReLU > 0, actualPreReLU, ConvertableTo[T].fromInt(0))
+  val linearOut = hiddenLayers(params.nLayers-1).zip(
+    io.midAndOutputWeights((params.nLayers-1)*params.nNeurons)).map{ case (a,b) => a * b}.reduce(_ + _) +
+    io.outputBias
 
-  val finalPredict = Wire(UInt(1.W))
-  finalPredict := Mux(actualVotes > 0, 1.U, 0.U)
+  val actualVotes = Mux(linearOut > 0, linearOut, ConvertableTo[T].fromInt(0))
+  val finalPredict = Mux(actualVotes > 0, 1.U, 0.U)
 
   // output probing
   val rawVotesReg = RegEnable(actualVotes, io.in.valid)
--- src/test/scala/NeuralNetSpec.scala
+++ src/test/scala/NeuralNetSpec.scala
@@ -7,17 +7,35 @@ import chisel3.core.FixedPoint
 class NeuralNetSpec extends FlatSpec with Matchers {
   behavior of "NeuralNet"
 
-  it should "work with random Floats" in {
+  it should "work with random Floats, more than one hidden layer" in {
     val debug = 0
 
     val dataWidth = 32
     val dataBP = 8
 
-    for (i <- 0 until 15) {
+    for (i <- 0 until 10) {
       val params = new NeuralNetParams[FixedPoint] {
         val protoData = FixedPoint(dataWidth.W,dataBP.BP)
         val nFeatures = scala.util.Random.nextInt(5) + 1
         val nNeurons = scala.util.Random.nextInt(20) + 1
+        val nLayers = scala.util.Random.nextInt(5) + 2
+      }
+      FixedPointNeuralNetTester(params, dataBP, debug) should be(true)
+    }
+  }
+
+  it should "work with random Floats, one hidden layer" in {
+    val debug = 0
+
+    val dataWidth = 32
+    val dataBP = 8
+
+    for (i <- 0 until 10) {
+      val params = new NeuralNetParams[FixedPoint] {
+        val protoData = FixedPoint(dataWidth.W,dataBP.BP)
+        val nFeatures = scala.util.Random.nextInt(5) + 1
+        val nNeurons = scala.util.Random.nextInt(20) + 1
+        val nLayers = 1
       }
       FixedPointNeuralNetTester(params, dataBP, debug) should be(true)
     }
